7206de0e243642b65dc3c2fd9421f363aff142fd
Filip Glowicki <filip.glowicki@outlook.com>
2025-04-13 17:49:34 +0200

    feat: Add code from next.js with supabase template

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default async function Home() {
+  return (
+    <main className="flex-1 flex flex-col gap-6 px-4">
+      <h1 className="text-2xl font-bold">Next.js + Supabase starter</h1>
+    </main>
+  );
+}
